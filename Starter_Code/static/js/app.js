b2b72153d4931f6d3af4f85c3d5e37ae43182595
pbrens <Brennan_Patrick2@bah.com>
2022-12-22 10:18:00 -0600

    updated w more funcs

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,33 +1,38 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-
-
     // display default
     function init() {
+        // pull data from json and store as variables
         samples = d3.json(url)
         samples.then(function(d){
         let names= d.names;
         let metadata = d.metadata;
         let samples = d.samples;
+
+        // set up dropdwon menu
         var dropdownMenu = d3.select("#selDataset");
         for (let i = 0; i < names.length; i++){
         dropdownMenu.append('option').text(names[i])
-        }
-
+        };
+        
+        // pull data for bar chart 
         let sortedValues = SortNSlice(samples[0]);
         
+        //set up inital bar cahrt
         let bardata = [{
             x: sortedValues.map(object => object.sample_values),
             y: sortedValues.map(object => object.otu_id),
-            text: sortedValues.map(object => object.otu_id),
+            text: sortedValues.map(object => object.otu_label),
             type: "bar",
             orientation: "h"
         }];
         
-        Plotly.newPlot('bar', bardata)
+        Plotly.newPlot('bar', bardata);
 
-        let allValues = PullAll(samples[0])
+        // pull data for bubble chart
+        let allValues = PullAll(samples[0]);
         
+        // set up initial bubble chart
         let bubbledata = [{
             x: allValues.map(object => object.otu_id),
             y: allValues.map(object => object.sample_values),
@@ -39,23 +44,58 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
                 size: allValues.map(object => object.sample_values)
             },
             type: 'bubble'
-        }]
+        }];
     
-        Plotly.newPlot('bubble', bubbledata)
+        Plotly.newPlot('bubble', bubbledata);
 
-        updateMetadata(samples[0].id,metadata)
+        // update metadata
+        updateMetadata(samples[0].id,metadata);
 
-
-        //
+        // set up guage chart
+        var data = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                title: { text: "Belly Button Washing Frequency" },
+                marker: {size: 28},
+                type: "indicator",
+                mode: "gauge+number",
+                value: metadata[0].wfreq,
+                gauge: {
+                    axis: {
+                        range:[null, 9],
+                        tickmode: 'array',
+                        tickvals: [1,2,3,4,5,6,7,8,9],
+                        ticks:'outside'
+                    },
+                    steps: [
+                        {range: [0,1], color:'Ivory'},
+                        {range: [1,2], color:'LightGoldenRodYellow'},
+                        {range: [2,3], color:'Beige'},
+                        {range: [3,4], color:'HoneyDew'},
+                        {range: [4,5], color:'DarkSeaGreen'},
+                        {range: [5,6], color:'CadetBlue'},
+                        {range: [6,7], color:'DarkGreen'},
+                        {range: [7,8], color:'DarkOliveGreen'},
+                        {range: [8,9], color:'DarkSlateGray'},
+                    ]
+                }
+            }
+        ];
         
+        var layout = { 
+            width: 600, 
+            height: 500, 
+            margin: { t: 0, b: 0 }
+        };
+        Plotly.newPlot('gauge', data, layout);
 
-    })
+        })
 
 
     }
     // function for when dropdown selection changes
     function optionChanged(id) {
-        samples = d3.json(url)
+        samples = d3.json(url);
         samples.then(function(d){
         let samples = d.samples;
         let metaData = d.metadata;
@@ -63,19 +103,19 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
         for (i = 0; i < samples.length; i++){
             if(samples[i].id == id){
                
-            var newBarValues = SortNSlice(samples[i])
+            var newBarValues = SortNSlice(samples[i]);
 
             var newBubbleValues = PullAll(samples[i])
-            }
-        }
+            };
+        };
         // update bar chart
-        updateBarChart(newBarValues)
+        updateBarChart(newBarValues);
 
         // update meta data
-        updateMetadata(id, metaData)
+        updateMetadata(id, metaData);
 
         // update bubble chart
-        updateBubbleChart(newBubbleValues)
+        updateBubbleChart(newBubbleValues);
         
         })
     }
@@ -88,7 +128,7 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 
         x = values.map(object => object.sample_values);
         y = values.map(object => object.otu_id);
-        text = values.map(object => object.otu_id);
+        text = values.map(object => object.otu_label);
         
         Plotly.restyle('bar', 'x', [x]);
         Plotly.restyle('bar', 'y', [y]);
@@ -148,7 +188,7 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
                 otu_id: `OTU ${values.otu_ids[i]}`,
                 otu_label: values.otu_labels[i]
             }
-            dicts.push(dict)
+            dicts.push(dict);
         }
         // sort the values
         let sorted = dicts.sort(function compareFunction(a,b){
@@ -156,11 +196,11 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
         })
 
         // set top 10 bacterias slice to a var
-        let top10 = sorted.slice(0,10)
+        let top10 = sorted.slice(0,10);
         
-        top10.reverse()
+        top10.reverse();
         // return top 10 as fucntion output
-        return top10
+        return top10;
         
         
     };
@@ -184,6 +224,6 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
     return dicts
     }
 
-    init()
+init()
 
 
